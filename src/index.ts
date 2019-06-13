0249dd8bf76a7acff1b2e44d42f20bbf80e4dcd2
turinglabsorg <turinglabs@tuta.io>
2019-06-13 08:52:26 +0200

    process management and .env example

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,13 @@ import * as Crypto from './libs/Crypto'
 import * as Daemon from "./libs/Daemon"
 import * as Database from "./libs/Database"
 const r = require('rethinkdb')
+const exec = require('child_process')
 
 let {nextAvailable} = require('node-port-check')
 require('dotenv').config()
 const axios = require('axios')
 
-const idanodejs = async () => {
+const nodeprocess = async () => {
   let port = await nextAvailable(3001, '0.0.0.0')
   app.engine('html', require('ejs').renderFile);
   
@@ -16,14 +17,34 @@ const idanodejs = async () => {
     if (err) {
       return console.log(err)
     }
-    var wallet = new Crypto.Wallet;
+    runIdaNode()
+    return console.log(`Scrypta IdaNode listening at port ${port}.`)
+      
+  })
+}
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+async function runIdaNode(){
+  var wallet = new Crypto.Wallet;
     wallet.request('getinfo').then( async function(info){
-      if(info !== undefined){
+      if(info !== undefined && info['result'] !== null && info['result']['blocks'] >= 0){
         console.log(process.env.COIN + ' wallet successfully connected.')
-        r.connect({ host: process.env.DB_HOST, port: process.env.DB_PORT }, async function(err, conn) {
-          if(err){
-            console.log('Can\'t communicate with database, please check.')
-          }else{
+        var dbconnected = false
+        while(dbconnected === false){
+          var conn = await r.connect({ host: process.env.DB_HOST, port: process.env.DB_PORT }).catch(async err => {
+            console.log('Can\'t communicate with database, running process now.')
+            exec.spawn('rethinkdb',{
+              stdio: 'ignore',
+              detached: true
+            }).unref()
+            console.log('Waiting 5 seconds, then try again.')
+            await sleep(5000)
+          })
+          if(conn !== undefined){
+            dbconnected = true
             console.log('Starting database and tables check.')
             var DB = new Database.Management
             var result = await DB.check()
@@ -33,13 +54,18 @@ const idanodejs = async () => {
             task = new Daemon.Sync
             task.init()
           }
-        })
+        }
       }else{
-        console.log('Can\'t communicate with wallet, please check RPC.')
+        console.log('Can\'t communicate with wallet, running process now.')
+        exec.spawn(process.env.LYRAPATH + '/lyrad',{
+          stdio: 'ignore',
+          detached: true
+        }).unref()
+        console.log('Waiting 5 seconds, then restart IdaNode.')
+        await sleep(5000)
+        runIdaNode()
       }
     })
-    return console.log(`Scrypta IdaNode listening at port ${port}.`)
-  })
 }
 
-idanodejs()
+nodeprocess()
