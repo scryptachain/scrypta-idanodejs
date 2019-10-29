a0118350744058dbaed6a5b60a86659808a1e5b0
turinglabsorg <turinglabs@criptext.com>
2019-10-29 12:05:40 +0100

    min fixes

--- install_rasp.sh
+++ install_rasp.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 #INSTALL WALLET DEPENDENCIES
+sudo apt-get install -y software-properties-common
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C70EF1F0305A1ADB9986DBD8D46F45428842CE5E
 echo 'deb http://ppa.launchpad.net/bitcoin/bitcoin/ubuntu bionic main' >> /etc/apt/sources.list
 echo 'deb-src http://ppa.launchpad.net/bitcoin/bitcoin/ubuntu bionic main' >> /etc/apt/sources.list
 echo 'deb http://security.debian.org/debian-security stretch/updates main' >> /etc/apt/sources.list
-sudo apt-get install -y software-properties-common python-software-properties
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 sudo apt-get -y install build-essential autoconf automake libboost-all-dev libleveldb-dev libgmp-dev libgmp3-dev libssl-dev libcurl4-openssl-dev libcrypto++-dev libqrencode-dev libminiupnpc-dev autogen libtool git libevent-dev libprotobuf-dev
 sudo apt-get install -y curl g++ git-core pkg-config libtool faketime bsdmainutils mingw-w64 g++-mingw-w64 nsis zip ca-certificates python
-sudo apt-get install -y libzmq3-dev
-sudo apt-get install -y libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
-sudo apt-get install -y libqrencode-dev
-apt-get install libssl1.0-dev
+sudo apt-get install libzmq3-dev
+sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+sudo apt-get install libqrencode-dev
+sudo apt install libssl1.0-dev
 
 #DOWNLOADING WALLET
 git clone https://github.com/scryptachain/scrypta
