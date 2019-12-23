7017f40130fd348fbf5451c63fa856e39863f1b2
turinglabsorg <turinglabs@criptext.com>
2019-12-23 07:48:52 +0100

    min fix

--- src/libs/Sidechain.ts
+++ src/libs/Sidechain.ts
@@ -35,9 +35,9 @@ module SideChain {
                         response(true)
                     }else{
                         let check_reissue = await db.collection('sc_transactions').find({sxid: sxid}).sort({block: 1}).toArray()
-                        let check_sidechain = await db.collection('written').find({ address: check_reissue[0].reissue.sidechain }).sort({ block: 1 }).limit(1).toArray()
-                        console.log('CHECK_REISSUE', sxid)
                         if(check_reissue[0] !== undefined){
+                            let check_sidechain = await db.collection('written').find({ address: check_reissue[0].reissue.sidechain }).sort({ block: 1 }).limit(1).toArray()
+                            console.log('CHECK_REISSUE', sxid)
                             if(check_reissue !== undefined && check_reissue[0] !== undefined && check_reissue[0].reissue !== undefined && check_reissue[0].sxid === sxid && check_reissue[0].reissue.owner === check_sidechain[0].data.genesis.owner){
                                 response(true)
                             }else{
