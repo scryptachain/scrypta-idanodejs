46133cfa56db7f0076802f47131c13b75719ab1b
turinglabsorg <turinglabs@criptext.com>
2020-01-16 10:33:11 +0100

    ui changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,17 @@
 var app = new Vue({
     el: '#app',
     data: {
+        show: 'explorer',
+        address: '',
+        newaddress: {},
         status: {},
         block: {
             outputs: 0
         },
         height: 0,
         last: 0,
+        written: '',
+        received: '',
         axios: window.axios,
         showRaw: false
     },
@@ -17,15 +22,49 @@ var app = new Vue({
         response = await app.axios.get('/block/last')
         app.block = response.data.data
         app.height = app.block.height
+        app.written = app.block.data_written
+        app.received = app.block.data_received
         app.last = app.height - 1
     },
     methods: {
+        searchBlock(){
+            const app = this
+            app.showBlock(app.height)
+        },
         async showBlock(last){
             const app = this
             response = await app.axios.get('/block/' + last)
             app.block = response.data.data
             app.height = app.block.height
+            app.written = app.block.data_written
+            app.received = app.block.data_received
             app.last = app.height - 1
+        },
+        async initAddress(){
+            const app = this
+            if(app.address !== ''){
+                response = await app.axios.post('/init', { address: app.address })
+                if(response.data.data.airdrop_tx !== false  && response.data.data.airdrop_tx !== null){
+                    alert('Funds set correctly!')
+                }else{
+                    alert('This address received the faucet yet.')
+                }
+            }else{
+                alert('Write an address first.')
+            }
+        },
+        async createAddress(){
+            const app = this
+            response = await app.axios.get('/wallet/getnewaddress')
+            app.newaddress = response.data
+        },
+        showExplorer(){
+            const app = this
+            app.show = 'explorer'
+        },
+        showTools(){
+            const app = this
+            app.show = 'tools'
         }
     }
 })
