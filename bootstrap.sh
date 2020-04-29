8b1ed0dfb057092cce6f92cb8a8f83ed7ad57f1a
turinglabsorg <turinglabs@criptext.com>
2020-04-29 06:37:34 +0200

    bootstrap script fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 wget https://sfo2.digitaloceanspaces.com/scrypta/idanode_bootstrap.gz
-pkill mongod
+killall mongod
 rm -rf mongodb_data
 mkdir mongodb_data
 rm -rf idanodejs
 tar -xvzf idanode_bootstrap.gz --strip-components 1
-sleep 10s
+sleep 20s
 mongod --dbpath=./mongodb_data &
 sleep 20s
 sudo mongorestore --db idanodejs --drop idanodejs
 rm -rf idanodejs
-pkill mongod
+killall mongod
