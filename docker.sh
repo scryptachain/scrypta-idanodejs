d9c73be9a7d29f7f668496b9bcfd75e469d9cdbe
turinglabsorg <s.cataudo@turinglabs.org>
2020-12-09 10:28:08 +0100

    fix docker file and script

--- docker.sh
+++ docker.sh
@@ -3,3 +3,6 @@
 cd docker
 docker build -t scrypta:idanode .
 docker run -d --name=idanode -dit -p 3001:3001 scrypta:idanode
+sleep 30
+docker exec -it idanode lyrad &
+docker exec -it -w /opt/scrypta-idanodejs idanode pm2 start dist/index.js
