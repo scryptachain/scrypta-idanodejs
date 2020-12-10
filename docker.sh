4d8d8baf917616d32a0cc595bf39d2a134c1cdf1
turinglabsorg <s.cataudo@turinglabs.org>
2020-12-10 09:42:45 +0100

    fixing scripts

--- docker.sh
+++ docker.sh
@@ -3,7 +3,7 @@
 cd docker
 docker build -t scrypta:idanode .
 
-if [ -z "$1" ]    
+if [ -z "$1" ]
 then
     echo "Running Scrypta IdaNode inside Docker, in mainnet mode."
     docker run --restart=unless-stopped -d --name=idanode -dit -p 3001:3001 scrypta:idanode
--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ npm run build
 if [ -z "$1" ]    
 then
     echo "Running Scrypta IdaNode in mainnet mode"
-    pm2 start npm -- start
+    npm run start
 else
     echo "Running Scrypta IdaNode in testnet mode"
-    pm2 start npm -- run start:testnet
+    npm run start:testnet
 fi
