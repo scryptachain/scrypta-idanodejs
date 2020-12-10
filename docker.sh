0ea8a12edadcb79dcae3ee5da424ebaa49f2526d
turinglabsorg <s.cataudo@turinglabs.org>
2020-12-10 11:01:40 +0100

    restarting node

--- docker.sh
+++ docker.sh
@@ -16,4 +16,5 @@ else
     echo "Running Scrypta IdaNode inside Docker, in mainnet mode."
     docker run --restart=unless-stopped -d --name=idanode -dit -p 3001:3001 scrypta:idanode
     docker exec -it -w /opt/ idanode bash bootstrap.sh
+    docker restart idanode
 fi
