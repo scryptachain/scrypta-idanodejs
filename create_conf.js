f679ba55f8dfc067e78344a77da63e4914b39374
turinglabsorg <s.cataudo@turinglabs.org>
2020-12-11 15:01:09 +0100

    changing conf folder

--- create_conf.js
+++ create_conf.js
@@ -10,7 +10,7 @@ async function create() {
     let rpcuser = await scrypta.createAddress('-')
     let idanodekey = await scrypta.createAddress('-')
 
-    let lyraconfcontent = `rpcuser=` + rpcuser.pub + `\nrpcpassword=` + rpcuser.prv + `\nrpcallowip=127.0.0.1\nlisten=1\nserver=1\ndaemon=1\nindex=1\ntxindex=1\nlogtimestamps=1`
+    let lyraconfcontent = `rpcuser=` + rpcuser.pub + `\nrpcpassword=` + rpcuser.prv + `\nrpcallowip=127.0.0.1\nlisten=1\nserver=1\ndaemon=1\nindex=1\ntxindex=1\nlogtimestamps=1\ndatadir=/opt/data/.lyra`
     let idanodeconfcontent = `RPCUSER=` + rpcuser.pub + `\nRPCPASSWORD=` + rpcuser.prv + `\nRPCPORT=42223\nRPCPORT_TESTNET=51475\nRPCADDRESS=localhost\nDEBUG=true\nDB_PORT=28015\nDB_HOST=localhost\nCOIN=LYRA\nSYNC=true\nAIRDROP=0\nSERVERMODE=true\nTESTNET=false\nP2PPORT=42226\nNODE_KEY=` + idanodekey.prv + `\nADMIN_PUBKEY=` + idanodekey.key + ``
 
     try {
