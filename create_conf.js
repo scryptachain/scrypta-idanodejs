deb13d982095e11aa662c1b031988c9fb023b121
turinglabsorg <s.cataudo@turinglabs.org>
2020-12-11 15:27:32 +0100

    conf fix

--- create_conf.js
+++ create_conf.js
@@ -2,7 +2,7 @@ const ScryptaCore = require('@scrypta/core')
 const scrypta = new ScryptaCore
 const fs = require('fs')
 const homedir = require('os').homedir();
-const lyraconf = require('path').join(homedir, '.lyra') + '/lyra.conf'
+const lyraconf = '/opt/data/.lyra/lyra.conf'
 console.log('CONFIG PATH IS ' + lyraconf)
 const idanodeconf = './.env'
 
@@ -11,7 +11,7 @@ async function create() {
     let idanodekey = await scrypta.createAddress('-')
 
     let lyraconfcontent = `rpcuser=` + rpcuser.pub + `\nrpcpassword=` + rpcuser.prv + `\nrpcallowip=127.0.0.1\nlisten=1\nserver=1\ndaemon=1\nindex=1\ntxindex=1\nlogtimestamps=1\ndatadir=/opt/data/.lyra`
-    let idanodeconfcontent = `RPCUSER=` + rpcuser.pub + `\nRPCPASSWORD=` + rpcuser.prv + `\nRPCPORT=42223\nRPCPORT_TESTNET=51475\nRPCADDRESS=localhost\nDEBUG=true\nDB_PORT=28015\nDB_HOST=localhost\nCOIN=LYRA\nSYNC=true\nAIRDROP=0\nSERVERMODE=true\nTESTNET=false\nMONGODB_PATH=/opt/data/idanode\nP2PPORT=42226\nNODE_KEY=` + idanodekey.prv + `\nADMIN_PUBKEY=` + idanodekey.key + ``
+    let idanodeconfcontent = `RPCUSER=` + rpcuser.pub + `\nRPCPASSWORD=` + rpcuser.prv + `\nRPCPORT=42223\nRPCPORT_TESTNET=51475\nRPCADDRESS=localhost\nDEBUG=true\nDB_PORT=28015\nDB_HOST=localhost\nCOIN=LYRA\nAIRDROP=0\nSERVERMODE=true\nTESTNET=false\nMONGODB_PATH=/opt/data/idanode\nP2PPORT=42226\nNODE_KEY=` + idanodekey.prv + `\nADMIN_PUBKEY=` + idanodekey.key + `\nSYNC=false`
 
     try {
         fs.writeFileSync(lyraconf, lyraconfcontent)
