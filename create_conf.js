408f69e68fec344c6841b5a37207762c21c81e56
turinglabsorg <s.cataudo@turinglabs.org>
2020-12-11 15:36:54 +0100

    adding lyrafolder parmeter

--- create_conf.js
+++ create_conf.js
@@ -2,7 +2,7 @@ const ScryptaCore = require('@scrypta/core')
 const scrypta = new ScryptaCore
 const fs = require('fs')
 const homedir = require('os').homedir();
-const lyraconf = '/opt/data/.lyra/lyra.conf'
+const lyraconf = '/opt/data/lyra/lyra.conf'
 console.log('CONFIG PATH IS ' + lyraconf)
 const idanodeconf = './.env'
 
@@ -10,8 +10,8 @@ async function create() {
     let rpcuser = await scrypta.createAddress('-')
     let idanodekey = await scrypta.createAddress('-')
 
-    let lyraconfcontent = `rpcuser=` + rpcuser.pub + `\nrpcpassword=` + rpcuser.prv + `\nrpcallowip=127.0.0.1\nlisten=1\nserver=1\ndaemon=1\nindex=1\ntxindex=1\nlogtimestamps=1\ndatadir=/opt/data/.lyra`
-    let idanodeconfcontent = `RPCUSER=` + rpcuser.pub + `\nRPCPASSWORD=` + rpcuser.prv + `\nRPCPORT=42223\nRPCPORT_TESTNET=51475\nRPCADDRESS=localhost\nDEBUG=true\nDB_PORT=28015\nDB_HOST=localhost\nCOIN=LYRA\nAIRDROP=0\nSERVERMODE=true\nTESTNET=false\nMONGODB_PATH=/opt/data/idanode\nP2PPORT=42226\nNODE_KEY=` + idanodekey.prv + `\nADMIN_PUBKEY=` + idanodekey.key + `\nSYNC=false`
+    let lyraconfcontent = `rpcuser=` + rpcuser.pub + `\nrpcpassword=` + rpcuser.prv + `\nrpcallowip=127.0.0.1\nlisten=1\nserver=1\ndaemon=1\nindex=1\ntxindex=1\nlogtimestamps=1\ndatadir=/opt/data/lyra`
+    let idanodeconfcontent = `RPCUSER=` + rpcuser.pub + `\nRPCPASSWORD=` + rpcuser.prv + `\nRPCPORT=42223\nRPCPORT_TESTNET=51475\nRPCADDRESS=localhost\nDEBUG=true\nDB_PORT=28015\nLYRAFOLDER=/opt/data/lyra\nDB_HOST=localhost\nCOIN=LYRA\nAIRDROP=0\nSERVERMODE=true\nTESTNET=false\nMONGODB_PATH=/opt/data/idanode\nP2PPORT=42226\nNODE_KEY=` + idanodekey.prv + `\nADMIN_PUBKEY=` + idanodekey.key + `\nSYNC=true`
 
     try {
         fs.writeFileSync(lyraconf, lyraconfcontent)
