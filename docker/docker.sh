35ab780f89a0c42b890f856aa0ce473f567570cb
turinglabsorg <s.cataudo@turinglabs.org>
2021-01-19 11:04:41 +0100

    min fix

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -16,7 +16,7 @@ sudo add-apt-repository \
    stable"
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 cd docker
 if [[ "$@" =~ "-rebuild" ]]
